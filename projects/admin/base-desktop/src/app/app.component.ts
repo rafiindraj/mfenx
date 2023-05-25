88a99575116bdfa40f2f3163297a53cfcd192c4d
rafiindraj <99.rafiindrajati@gmail.com>
2023-05-25 16:14:59 +0700

    add projects

---
+++ projects/admin/base-desktop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfenx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'projects-admin-base-desktop';
+}
